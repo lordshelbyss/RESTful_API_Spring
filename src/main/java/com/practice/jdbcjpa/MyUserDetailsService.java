f193d37626a2795001131fe2299a29df5f4898f3
Sam <sahasamarpan01@gmail.com>
2021-07-27 19:42:40 +0530

    Google SSO-exception while running

--- src/main/java/com/practice/jdbcjpa/MyUserDetailsService.java
+++ src/main/java/com/practice/jdbcjpa/MyUserDetailsService.java
@@ -24,3 +24,5 @@ public class MyUserDetailsService implements UserDetailsService {
 	}  
 
 }
+
+
--- src/main/java/com/practice/jdbcjpa/SecurityConfiguration.java
+++ src/main/java/com/practice/jdbcjpa/SecurityConfiguration.java
@@ -4,14 +4,14 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -20,32 +20,33 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	UserDetailsService userDetailsService;
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		// TODO Auto-generated method stub
-//		auth.inMemoryAuthentication()
-//			.withUser("Sam")
-//			.password("Sam")
-//			.roles("User");
-		
-		
-		// Using userdetailsservice
-		auth.userDetailsService(userDetailsService);	
-		
-			
-	}
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		
+//		// TODO Auto-generated method stub
+////		auth.inMemoryAuthentication()
+////			.withUser("Sam")
+////			.password("Sam")
+////			.roles("User");
+//		
+//		
+//		// Using userdetailsservice
+//		auth.userDetailsService(userDetailsService);	
+//		
+//			
+//	}
+	
 	
 	
 // Does authorisation -- role based access 
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		// TODO Auto-generated method stub
-//		http.authorizeRequests()
-//			.antMatchers("/note_taking_app/notes").hasRole("User")
-//			.antMatchers("note_taking_app").permitAll();
-//	}
-//	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and().oauth2Login();
+	}
+	
 	
 	// Encoding password into database
 	// Explore several encoders
