a503192aab178c7edae323934483d58c464564ba
Sam <sahasamarpan01@gmail.com>
2021-07-28 15:08:00 +0530

    OAuthUser file changed

--- src/main/java/com/practice/jdbcjpa/CustomOAuth2UserService.java
+++ src/main/java/com/practice/jdbcjpa/CustomOAuth2UserService.java
@@ -1,13 +1,13 @@
 package com.practice.jdbcjpa;
 
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomOAuth2UserService
-		extends org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService {
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
